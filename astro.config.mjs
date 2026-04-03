35dfd5273879b369caccda68091ac1138128b375
invest-at-13 <invest-at-13@users.noreply.github.com>
2026-04-03 11:10:32 +0900

    Cloudflare Pages用にシンプルな静的サイトに戻す

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,16 +4,11 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
-import cloudflare from '@astrojs/cloudflare';
-
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://invest-at-13.sizuk-h-777.workers.dev',
-
-  integrations: [
-      mdx(),
-      sitemap(),
+	site: 'https://invest-at-13.pages.dev',
+	integrations: [
+		mdx(),
+		sitemap(),
 	],
-
-  adapter: cloudflare(),
 });
