bc9ab653c73ba012534bb1ab743982616469481b
invest-at-13 <invest-at-13@users.noreply.github.com>
2026-04-03 10:24:45 +0900

    Cloudflare Pages用にサイトURL変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://invest-at-13.github.io',
-	base: '/invest-at-13',
+	site: 'https://invest-at-13.pages.dev',
 	integrations: [
 		mdx(),
 		sitemap(),
